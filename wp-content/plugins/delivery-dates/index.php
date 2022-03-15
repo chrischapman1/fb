07d6323f7c603a399fd370c3dbe95d18c32cc2bd
chrischapman1 <chris@clickk.com.au>
2022-03-15 13:31:41 +1100

    delivery plugin + date selector initialise

---
+++ wp-content/plugins/delivery-dates/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Delivery Dates
+ * Plugin URI: http://www.clickk.com.au
+ * Description: Delivery date selector
+ * Version: 1.0
+ * Author: Your Name
+ * Author URI: http://www.clickk.com.au
+ */
+
+ ?>
---
+++ wp-content/plugins/run-sheets/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Run Sheets
+ * Plugin URI: http://www.clickk.com.au
+ * Description: Delivery run sheets for FB orders
+ * Version: 1.0
+ * Author: Your Name
+ * Author URI: http://www.clickk.com.au
+ */
+
+ ?>
